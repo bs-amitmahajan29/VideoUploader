dfa565ddb91362d11ca90f5fdc84184a4763e96e
Amit Mahajan <amit.m@browserstack.com>
2024-12-22 10:56:35 +0530

    feat: Added route `/upload` to move form_data file into a folder

--- video_uploader.py
+++ video_uploader.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, Header
+from fastapi import FastAPI, File, UploadFile, HTTPException, Header
+import os
 import json
 
 with open('config/config.json') as config_file:
@@ -6,8 +7,12 @@ with open('config/config.json') as config_file:
 
 API_TOKENS = config['api_token']
 
+UPLOAD_FOLDER = 'uploads'
+DB_FILE = 'config/video_uploader.db'
+
 # Initialize app
 app = FastAPI()
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 def authenticate(api_token: str = Header(None)):
     if not api_token:
@@ -21,3 +26,15 @@ async def check_api_token(api_token: str = Header(None)):
     authorized = authenticate(api_token)
     return {"Authorized": authorized}
 
+@app.post("/upload")
+async def upload_video(file: UploadFile = File(...), api_token: str = Header(None)):
+    authenticate(api_token)
+
+    await file.seek(0)
+    filename = file.filename
+    filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+    with open(filepath, "wb") as f:
+        f.write(await file.read())
+
+    return {"filename": filename}
